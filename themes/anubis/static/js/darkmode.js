1c0a6ce6801ac6532ec49fc055e7549fdde85d38
Pranav Goyanka <pgoyanka@gmail.com>
2020-05-07 18:24:31 +0530

    Fix hr tags in dark mode

--- themes/anubis/static/js/darkmode.js
+++ themes/anubis/static/js/darkmode.js
@@ -27,9 +27,3 @@ var toggle = document.getElementById("dark-mode-toggle");
 
     var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
 setTheme(savedTheme);
-
-// function setTheme(mode) {
-//     localStorage.setItem("dark-mode-storage", mode);
-
-//     // same as above
-// }
